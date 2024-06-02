a5ca2c54188dc68f14a87cff8833840596ac655d
clibroia <carmine87.italy@gmail.com>
2024-06-02 07:29:18 -0400

    Task 3 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,34 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = parseInt(req.params.isbn);
+  if(isbn in books) {
+    return res.send({
+        isbn: isbn,
+        ...books[isbn]
+    });
+  }
+  return res.status(404).json({message: "Book not found"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let authorbooks = [];
+  for(const isbn in books) {
+    if(books[isbn]["author"] === author) {
+        authorbooks.push({isbn: isbn, ...books[isbn]});
+    }
+  }
+  if(authorbooks.length > 0) {
+    return res.send(authorbooks);
+  }
+  return res.status(404).json({message: "No books of the requested author"});
 });
 
 // Get all books based on title
